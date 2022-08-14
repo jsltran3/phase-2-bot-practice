8f87159b42dddb9590c0df600a7b3fba4f675ce8
Joe Tran <33337835+jsltran3@users.noreply.github.com>
2022-08-14 13:28:54 -0700

    Update BotsPage.js
    add my code

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,31 +1,72 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  // start here with your code for step one
-  const [collection, setCollection] = useState([]);
-  const [army, setArmy] = useState([])
-  
-    useEffect(() => {
-      fetch('http://localhost:3001/bots')
-      .then(resp => resp.json())
-      .then(collection => setCollection(collection))
-    }, [])
+  const [getBot, setGetBot] = useState([])
+  const [selectArmyBot, setSelectArmyBot] = useState([])
 
-    function handleDraftBot(bot){
-        setArmy([...army, bot])
+  useEffect(() => {
+    fetch("http://localhost:8002/bots"
+    , {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
     }
+    )
+      .then((resp) => resp.json())
+      .then((bots) => {
+        const updatedBots = bots.map((bot) => {
+          return {...bot, drafted: false};
+        });
+        setGetBot(updatedBots)
+        console.log(updatedBots)
+      })
+  }, [])
 
-    function handleDeleteBot(id) {
-     const updatedArmyArray= army.filter((bot) => bot.id !== id);
-        setArmy(updatedArmyArray);
-    }
-  
+
+
+
+
+  // function handleBotClick(bot) {
+  //   const arr = setSelectArmyBot([...selectArmyBot, bot]);
+  //   const arrSame = () => arr == bot ? (setSelectArmyBot([...selectArmyBot, bot])) : ""
+  //   return arrSame(bot);
+  // }
+
+  function handleBotClick(draftedBot) {
+    
+    // const updatedBot = selectArmyBot.map((draftee) => {
+    const updatedBot = getBot.map((toBeDrafted) => {
+      if (toBeDrafted.id === draftedBot.id) return {...toBeDrafted, drafted: true };
+      console.log("I was drafted")
+      return toBeDrafted;
+      
+    })
+
+    setGetBot(updatedBot)
+    console.log("Got to the bottom")
+    console.log(updatedBot)
+  }
+
+  const alreadyDrafted = getBot.filter((bot) => bot.drafted);
+
+
   return (
     <div>
-      <YourBotArmy army={army} onDeleteBot={handleDeleteBot}/> 
-      <BotCollection collection={collection} onDraftBot={handleDraftBot}/> 
+      <YourBotArmy 
+        getBot={getBot}
+        setGetBot={setGetBot}
+        selectArmyBot={selectArmyBot}
+        setSelectArmyBot={setSelectArmyBot}
+        handleBotClick={handleBotClick}
+        alreadyDrafted={alreadyDrafted}
+      />
+      <BotCollection 
+        getBot={getBot}
+        handleBotClick={handleBotClick}
+      />
     </div>
   )
 }
