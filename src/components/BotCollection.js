e0c590a0ec5b71805849bcebbb0ec0a23b0c2d66
Joe Tran <33337835+jsltran3@users.noreply.github.com>
2022-08-14 13:28:38 -0700

    Update BotCollection.js
    my code

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,19 +1,44 @@
 import React from "react";
-import BotCard from "./BotCard";
+import BotCard from "./BotCard"
 
-function BotCollection({collection, onDraftBot, onDeleteBot}) {
-  // Your code here
+function BotCollection({ getBot, handleBotClick }) {
 
-  const botList = collection.map((bot) => {
-    return <BotCard key={bot.id} bot={bot} onDraftBot={onDraftBot} onDeleteBot={onDeleteBot}/>
-  })
+  const renderBotList = getBot.map(bot =>
+    <BotCard 
+      bot={bot}
+      id={bot.id}
+      key={bot.id}
+      handleBotClick={handleBotClick}
+    />);  
+
+  const renderFourBots = renderBotList.filter((e,k) => k < 4);
+
+  // const renderRandomBots = (randoBot,num) => {
+  //   // const randomBotIndex = Math.floor(Math.random() * randoBot.length);
+
+  //   const randoBots = [...randoBot].sort(() => 0.5 - Math.random());
+
+  //   // const randoBots = randoBot[randomBotIndex];
+
+  //   return randoBots.slice(0, num);
+  //   // return randoBots;
+  //   }
+  
+  // function handleClickBotMove(id) {
+  //   const updatedBots = renderFourBots.filter((bots) => bots.id !== id);
+  //   console.log(updatedBots);
+  //   setSelectArmyBot(updatedBots);
+  // }
+    
 
   return (
     <div className="ui four column grid">
       <div className="row">
         Collection of all bots
-        {botList}
+        {/* {renderRandomBots(renderFourBots, 4)} */}
+        
       </div>
+      {renderFourBots}
     </div>
   );
 }
